77aac34374b0d3b9924fd2f0126d807c67c60d09
erik@ejnelson.com <christamichels@Christas-MacBook-Pro.local>
2017-01-08 14:33:01 -0600

    commit project

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var expenditures=0;
+
+
 $(function () {
   console.log('document is ready');
 
@@ -16,14 +19,29 @@ $(function () {
     appendDom(formData);
 
     clearForm();
+
+    salary(formData.employeeSalary);
   });
+
+
 });
 
 function appendDom(emp) {
   var $emp = $('<div class="employee"></div>'); // create a div jQuery object
-
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+  var $ul= $('<ul></ul>');
+  var $firstName=$('<li>' + emp.employeeFirstName + '</li>' );
+  var $lastName=$('<li>' + emp.employeeLastName + '</li>' );
+  var $id=$('<li>' + emp.employeeIdNumber + '</li>' );
+  var $job=$('<li>' + emp.employeeJobTitle + '</li>' );
+  var $salary=$('<li id="salary">' + emp.employeeSalary + '</li>' );
+  var $button=$('<li><button id="delete">del</button></li>')
+  $ul.append($firstName);
+  $ul.append($lastName);
+  $ul.append($id);
+  $ul.append($job);
+  $ul.append($salary);
+  $ul.append($button);
+  $emp.append($ul);
 
   $('#employees').append($emp); // append our div to the DOM
 }
@@ -31,3 +49,19 @@ function appendDom(emp) {
 function clearForm() {
   $('form').find('input[type=text]').val('');
 }
+
+function salary(add){
+  expenditures+=parseFloat((Number(add)/12).toFixed(2));
+  //expenditures=expenditures.toFixed(2);
+  $('#expenditures').text(expenditures);
+
+}
+$(document).on('click','#delete',function(){
+  console.log('delete');
+  var salary=Number($(this).parent().parent().find('#salary').text());
+  console.log(salary);
+  expenditures-=parseFloat((salary/12).toFixed(2));
+  //expenditures=expenditures.toFixed(2);
+  $('#expenditures').text(expenditures);
+  $(this).parent().parent().parent().remove();
+});
